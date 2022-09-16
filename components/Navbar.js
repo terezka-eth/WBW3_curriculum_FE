c3b922b66a01b15b844fbba061d7a8d69528b845
Tereza Burianova <40072706+T-ess@users.noreply.github.com>
2022-09-16 18:41:47 +0200

    Connect wallet + create event

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,8 +1,14 @@
 import { useState, useEffect } from "react";
 import Link from "next/link";
 import Navmenu from "./Navmenu";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import { useAccount, useDisconnect } from "wagmi";
 
 export default function Navbar() {
+  // connect or disconnect wallet
+  const { data: account } = useAccount();
+  const { disconnect } = useDisconnect();
+
   const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
@@ -28,6 +34,11 @@ export default function Navbar() {
                   Create Event
                 </a>
               </Link>
+              {account ? (
+                <Navmenu account={account} disconnect={() => disconnect()} />
+              ) : (
+                <ConnectButton />
+              )}
             </div>
           </div>
         </nav>
